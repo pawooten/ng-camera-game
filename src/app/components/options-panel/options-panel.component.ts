0bac25718663e3c9ca712dc616e163b7a9b2c311
pawooten@gmail.com <pawooten@gmail.com>
2021-09-10 20:14:06 -0400

    bound color toggles

--- src/app/components/options-panel/options-panel.component.ts
+++ src/app/components/options-panel/options-panel.component.ts
@@ -8,11 +8,29 @@ import {ThemePalette} from '@angular/material/core';
   styleUrls: ['./options-panel.component.css']
 })
 export class OptionsPanelComponent implements OnInit {
-  color: ThemePalette = 'primary';
-  constructor(private userPreferenceService: UserPreferenceService) { }
+  primaryThemeColor: ThemePalette = 'primary';
+
+  private readonly colorStates: { [color:string]: boolean} = {};
+  readonly picColors = [  { Label: 'Red', Value: 'red' },
+                          { Label: 'Green', Value: 'green' },
+                          { Label: 'Blue', Value: 'blue' },
+                          { Label: 'Yellow', Value: 'yellow' },
+                          { Label: 'Orange', Value: 'orange' },
+                          { Label: 'Gray', Value: 'gray' },
+                          { Label: 'White', Value: 'white' },
+                          { Label: 'Brown', Value: 'brown' },
+                          { Label: 'Black', Value: 'black' },
+                          { Label: 'Purple', Value: 'purple' },                        
+                        ];
+  constructor(private userPreferenceService: UserPreferenceService) {
+    this.picColors.forEach( color => this.colorStates[color.Value] = true);
+  }
 
   ngOnInit(): void {
     this.userPreferenceService.sayHello();
   }
 
+  isChecked(color: string): boolean {
+    return this.colorStates[color];
+  }
 }
