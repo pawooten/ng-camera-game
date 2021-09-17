43c53b0a488d591eb1121b368ef1ee2cdabeb6dd
pawooten@gmail.com <pawooten@gmail.com>
2021-09-16 20:33:53 -0400

    feat: routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing-module';
 import { HtmlCameraComponent } from './components/html-camera/html-camera.component';
 import { NgCameraComponent } from './components/ng-camera/ng-camera.component';
 import { OptionsPanelComponent } from './components/options-panel/options-panel.component';
 import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
 import { PicColorCanvasComponent } from './components/pic-color-canvas/pic-color-canvas.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
@@ -18,14 +20,16 @@ import { PicColorCanvasComponent } from './components/pic-color-canvas/pic-color
     NgCameraComponent,
     OptionsPanelComponent,
     ScoreboardComponent,
-    PicColorCanvasComponent
+    PicColorCanvasComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
     MatSliderModule,
     MatSlideToggleModule,
-    MatGridListModule
+    MatGridListModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
