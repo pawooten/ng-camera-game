d73518de766e3fa0f234a2f4c75d52ea6c296366
pawooten@gmail.com <pawooten@gmail.com>
2021-10-04 18:48:23 -0400

    feat: calculateRGBColorDistance() util

--- src/app/services/game-service.ts
+++ src/app/services/game-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PicColorState } from '../interfaces/PicColorState';
 import { RGB } from '../interfaces/RGB';
-import { RGBFromString } from '../utils';
+import { RGBFromString, calculateRGBColorDistance } from '../utils';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +15,13 @@ export class GameService {
         this.picSets = newPicSets;
         this.picSetIndex = 0;
     }
+
     examineColor(color: RGB) : void {
         console.log('color is ' + JSON.stringify(color));
         let picSet = this.picSets[this.picSetIndex];
         let picRGB = RGBFromString(picSet.Value);
         // how to diff ...
+        let distance = calculateRGBColorDistance(color, picRGB);
+        console.log(`distance=${distance}`);
     }
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -17,3 +17,16 @@ export function RGBFromString(stringValue: string) : RGB {
     const rIndex = 0, gIndex = 1, bIndex = 2;
     return { R: +stringValues[rIndex], G: +stringValues[gIndex], B: +stringValues[bIndex] };
 }
+
+export function calculateRGBColorDistance(colorA: RGB, colorB: RGB) : number {
+    // https://en.wikipedia.org/wiki/Color_difference Euclidean
+
+    if (!colorA || !colorB) {
+        throw new Error('null');
+    }
+    let rDelta = colorB.R - colorA.R;
+    let gDelta = colorB.G - colorA.G;
+    let bDelta = colorB.B - colorA.B;
+    let squaredDeltaSums = Math.pow(rDelta, 2) + Math.pow(gDelta, 2) + Math.pow(bDelta, 2);
+    return Math.sqrt(squaredDeltaSums);
+}
