0f6ec537e1938cb9aaa5772ac182a902a950e755
pawooten@gmail.com <pawooten@gmail.com>
2021-10-16 09:22:56 -0400

    refactor: PicAssignment: PicColorState -> PicColor

--- src/app/interfaces/pic-assignment.ts
+++ src/app/interfaces/pic-assignment.ts
@@ -1,7 +1,22 @@
-import { PicColorState } from "./pic-color-state";
+import { PicColor } from "./pic-color";
 
+/**
+ * Describes an assignment of a PicColor. Game instances are composed of a series
+ * of PicAssignment instances.
+ */
 export interface PicAssignment {
+    /**
+     * Describes the position of this instance relative to other PicAssignment instances.
+     * PicNumber 1 is he first PicAssignment of a Game. Once this assignment has been matched,
+     * the game progresses to PicNumber 2 and continues until all assignments of the Game are satisfied.
+     */
     PicNumber: number;
-    Color: PicColorState;
-    Score?: number;
+    /**
+     * The PicColor of this PicAssignment.
+     */
+    Color: PicColor;
+    /** 
+     * The Score the user has received for this PicAssignment, if any. 
+     */
+    Score: number;
 }
